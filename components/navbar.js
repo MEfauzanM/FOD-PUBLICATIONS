4d2341ef54b187358820eb2c2458049208d32e92
FAUZAN M <poomon376@gmail.com>
2025-07-17 11:31:38 +0545

    Delete components directory

--- components/navbar.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.getElementById("navbar");
-
-// navbar.innerHTML = `<h1>FOD PUBLICATION</h1>
-//         <ul class="links">
-//             <li><a href="index.html">Home</a></li>
-//             <li><a href="works/index.html">Works</a></li>
-//             <li><a href="author/index.html">Author</a></li>
-//         </ul>`       
