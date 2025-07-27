fdffff418bd6fcd652516a53d75eae775d37dbd1
MEfauzanM <poomon376@gmail.com>
2025-07-27 17:21:41 +0545

    now one

---
+++ scripts/components.js
@@ -0,0 +1,15 @@
+const navbar = document.getElementById("navbar")
+
+navbar.innerHTML = ` <div class="navbar">
+    <div class="logo"></div>
+    <div class="links">
+      <a href="index.html">Home</a>
+      <a href="works.html">Works</a>
+      <a href="authors.html">Author</a>
+      <a href="publish.html">Publish</a>
+    </div>
+    <a href="signup.html" id="signup">Sign-up</a>
+  </div>`
+
+  var body = document.getElementById("body")
+  if ( body.style.width <= ``)
